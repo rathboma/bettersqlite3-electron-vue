1f757d0ec3f6fe58b514a9ae3f0ed5a8cc0e42cc
Matthew Rathbone <matthew.rathbone@gmail.com>
2021-05-15 21:48:47 -0500

    updated example with externals

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,22 @@
 const ThreadsPlugin = require('threads-plugin');
 
+const webpackExternals = {
+  'better-sqlite3': 'commonjs better-sqlite3',
+  'tiny-worker': 'tiny-worker',
+  'node-pre-gyp': 'commonjs2 node-pre-gyp'
+}
+
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      externals: ['better-sqlite3'],
+      externals: ['better-sqlite3', 'node-pre-gyp'],
       nodeIntegration: true,
     },
   },
   configureWebpack: {
-    externals: {
-      'better-sqlite3': 'commonjs better-sqlite3'
-    },
+    externals: webpackExternals,
     plugins: [
-      new ThreadsPlugin({ globalObject: 'self', target: 'electron-node-worker' })
+      new ThreadsPlugin({ globalObject: 'self', target: 'electron-node-worker', externals: webpackExternals })
     ]
   }
 
